9c089712e05ae454a4a74a5a1cc25ffbe4824312
Arthur Dutra Alves <arthurdual2014@gmail.com>
2019-09-11 17:20:21 -0300

    adicionado botoes e modo noturno

--- js/javascript.js
+++ js/javascript.js
@@ -1,67 +1,128 @@
 /*jshint esversion: 6*/
+let btnFuncao = document.querySelectorAll('.func');
 let btnOperacao = document.querySelectorAll('.ope');
-//let btnNumero = document.querySelectorAll('.num');
-let btn1 = document.getElementById('#1');
-let btn2 = document.getElementById('#2');
-let btn3 = document.getElementById('#3');
-let btn4 = document.getElementById('#4');
-let btn5 = document.getElementById('#5');
-let btn6 = document.getElementById('#6');
-let btn7 = document.getElementById('#7');
-let btn8 = document.getElementById('#8');
-let btn9 = document.getElementById('#9');
-let btn0 = document.getElementById('#0');
+let btnNumero = document.querySelectorAll('.num');
 let btnIgual = document.querySelector('.igual');
+let visor = document.querySelector('input[name=visor]');
+/*
+    C é uma sigla para "clear" ("limpar", em inglês) e 
+    CE, para "cancel entry" ("cancelar registro"). 
+    M+, é para você colocar um número na memória. 
+    M-, é para você subtrair um número da memória.. 
+    vamos dar um ex. 
+    você digita o numero 100, e em seguida aperta a tecla M+ 
+    Você colocou o numero 100 na memória. 
+    digamos que agora você digitou o numero 30 e apertou a tecla M-, você esta subtraindo 30 de 100, portanto na memória ficará somente o numero 70. 
 
-btnIgual.onclick = function(){
-    alert("teste");
-};
-btn1.onclick = function(){
-    alert("1");
-};
-
-console.log(btnNumero);
-console.log(btnOperacao);
-
-/*let calcular = document.querySelector('button');
-let txt1 = document.querySelector('input[name=Valor1]');
-let txt2 = document.querySelector('input[name=Valor2]');
-let radio = document.querySelectorAll('input[name=operacoes]');
-let resut = document.querySelector('input[name=Resultado]');
-//console.log(radio);
-
-calcular.onclick = function(){
-    for (var i = 0; i < radio.length; i++) {
-        if (radio[i].checked) {
-            let v1 = parseFloat(txt1.value);
-            let v2 = parseFloat(txt2.value);
-            //console.log(v1);
-            switch (radio[i].id) {
-                case "som":
-                    //console.log("soma");
-                    //console.log (v1 + v2)
-                    resut.value = (v1 + v2);
+    Leia mais em Brainly.com.br - https://brainly.com.br/tarefa/5354688
+*/
+for( let i=0; i<btnFuncao.length; ++i ){
+    //console.log(i, btnFuncao[i].id);
+        btnFuncao[i].onclick = function(){
+            //console.log(i,btnFuncao[i].id);
+            switch (btnFuncao[i].id) {
+                case "OnOff":
+                    let on = true;
+                    //alert("on");
                     break;
-                case "sub":
-                    //console.log("subtração");
-                    //console.log (v1 - v2);
-                    resut.value = (v1 - v2);
+                case "(":
+                    visor.value += "(";
                     break;
-                case "div":
-                   //console.log("divisão");
-                    //console.log (v1 / v2);
-                    resut.value = (v1 / v2);
+                case ")":
+                    visor.value += ")";
+                     break;
+                case "CE":
+                    visor.value -= " ";
                     break;
-                case "mul":
-                    //console.log("multiplicação");
-                    //console.log (v1 * v2);
-                    resut.value = (v1 * v2);
+                case "C":
+                    visor.value = "";
+                    break;
+                case "M+":
+                    visor.value += "m+";
+                    break;
+                case "M-":
+                    visor.value += "m-";
+                    break;
+                case "MRC":
+                    visor.value += "MRC";
+                    break;
+            }
+        }
+}
+for( let i=0; i<btnNumero.length; ++i ){
+    //console.log(i, btnNumero[i].id);
+        btnNumero[i].onclick = function(){
+            //console.log(i,btnNumero[i].id);
+            switch (btnNumero[i].id) {
+                case "1":
+                    visor.value += 1;
+                    break;
+                case "2":
+                    visor.value += 2;
+                    break;
+                case "3":
+                    visor.value += 3;
+                    break;
+                case "4":
+                    visor.value += 4;
+                    break;
+                case "5":
+                    visor.value += 5;
+                    break;
+                case "6":
+                    visor.value += 6;
+                    break;
+                case "7":
+                    visor.value += 7;
+                    break;
+                case "8":
+                    visor.value += 8;
+                    break;
+                case "9":
+                    visor.value += 9;
+                    break;
+                case "0":
+                    visor.value += 0;
+                    break;
+                case "ponto":
+                    visor.value += ".";
                     break;
                 default:
                     break;
             }
-            //console.log(radio[i].id);
         }
+}
+
+for( let i=0; i<btnOperacao.length; ++i ){
+    //console.log(i, btnOperacao[i].id);
+        btnOperacao[i].onclick = function(){
+            //console.log(i,btnOperacao[i].id);
+            switch (btnOperacao[i].id) {
+                case "divi":
+                    visor.value += "/";
+                    break;
+                case "mult":
+                    visor.value += "*";
+                    break;
+                case "subt":
+                    visor.value += "-";
+                    break;
+                case "soma":
+                    visor.value += "+";
+                    break;
+                default:
+                    break;
+            }
+        }
+}
+document.addEventListener('keypress', function(e){
+    //console.log(e.which);
+    if(e.which == 13){
+        visor.value = eval(visor.value);
     }
+ });
+btnIgual.onclick = function(){
+    //console.log(eval(visor.value));
+    visor.value = eval(visor.value);
+    //visor.value = "";
 };
-*/
